d6a0c3b8e3c63664af5feb201ed8a0a235bee617
blademainer <blademainer@gmail.com>
2025-02-18 21:42:50 +0800

    init

---
+++ mpgs.py
@@ -0,0 +1,25 @@
+from scrapegraphai.graphs import SmartScraperGraph
+
+graph_config = {
+    "llm": {
+        "model": "deepseek-r1:70b",
+        "temperature": 0,
+        "format": "json",  # Ollama 需要显式指定格式
+        "base_url": "http://localhost:11434",  # 设置 Ollama URL
+    },
+    "embeddings": {
+        "model": "ollama/nomic-embed-text",
+        "base_url": "http://localhost:11434",  # 设置 Ollama URL
+    },
+    "verbose": True,
+}
+
+smart_scraper_graph = SmartScraperGraph(
+    prompt="List all the protocols that are supported by the API",
+    # 也接受已下载的 HTML 代码的字符串
+    source="https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US",
+    config=graph_config
+)
+
+result = smart_scraper_graph.run()
+print(result)
