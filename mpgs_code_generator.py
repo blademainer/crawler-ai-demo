26789d5284337434372852c44f99505d8cbdb4c2
blademainer <blademainer@gmail.com>
2025-02-19 15:40:33 +0800

    add code generator

--- mpgs_code_generator.py
+++ mpgs_code_generator.py
@@ -1,6 +1,20 @@
-from langchain_core.prompts import *
-from scrapegraphai.graphs import SmartScraperGraph
-from scrapegraphai.prompts import *
+import os
+from typing import List
+import json
+from dotenv import load_dotenv
+from pydantic import BaseModel, Field
+
+from scrapegraphai.graphs import CodeGeneratorGraph
+
+class Parameter(BaseModel):
+    name: str = Field(description="The name of the field")
+    description: str = Field(description="The description of the project")
+    type: str = Field(description="The type of the field, e.g. 'string', 'int', 'float', 'enum'")
+    parameters: List["Parameter"] = Field(description="The children of the field")
+
+
+class Parameters(BaseModel):
+    parameters: List[Parameter]
 
 graph_config = {
     "llm": {
@@ -8,21 +22,28 @@ graph_config = {
         "temperature": 0,
         "format": "json",  # Ollama 需要显式指定格式
         "base_url": "http://localhost:11434",  # 设置 Ollama URL
-        "max_tokens": 1000000,
-    },
-    "embeddings": {
-        "model": "ollama/nomic-embed-text",
-        "base_url": "http://localhost:11434",  # 设置 Ollama URL
+        "max_tokens": 100000,
     },
     "verbose": True,
+    "headless": False,
+    "reduction": 2,
+    "max_iterations": {
+        "overall": 10,
+        "syntax": 3,
+        "execution": 3,
+        "validation": 3,
+        "semantic": 3,
+    },
+    "output_file_name": "extra_mpgs_data.py",
 }
 
-smart_scraper_graph = SmartScraperGraph(
+code_generator_graph = CodeGeneratorGraph(
     prompt="Extract detailed information about the fields (with their children) in both the request body and response body for the following API: 'https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US'. Expected Output Format: A dictionary containing two keys, 'request_body' and 'response_body', where each key has a value that is another dictionary with the field names as keys and their respective children as values.",
     # 也接受已下载的 HTML 代码的字符串
     source="https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US",
-    config=graph_config
+    schema=Parameters,
+    config=graph_config,
 )
 
-result = smart_scraper_graph.run()
+result = code_generator_graph.run()
 json.dump(result, open("mpgs_api_fields.json", "w"), indent=4)
--- mpgs_with_openai.py
+++ mpgs_with_openai.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 from scrapegraphai.graphs import SmartScraperGraph
@@ -16,14 +17,15 @@ graph_config = {
         "voice": "alloy"
     },
     "output_path": "audio_summary.mp3",
+    "verbose": True,
 }
 
 smart_scraper_graph = SmartScraperGraph(
-    prompt="List all the fields of request body and response body for the API",
+    prompt="Extract detailed information about the fields (with their children) in both the request body and response body for the following API: 'https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US'. Expected Output Format: A dictionary containing two keys, 'request_body' and 'response_body', where each key has a value that is another dictionary with the field names as keys and their respective children as values.",
     # 也接受已下载的 HTML 代码的字符串
     source="https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US",
     config=graph_config
 )
 
 result = smart_scraper_graph.run()
-print(result)
+json.dump(result, open("mpgs_api_fields.json", "w"), indent=4)
