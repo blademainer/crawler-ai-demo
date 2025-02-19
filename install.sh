24b2588f5a251044414ba8bcdfef5c029f4e141d
blademainer <blademainer@gmail.com>
2025-02-19 11:04:02 +0800

    add openai model

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+python3 -m venv myenv
+source myenv/bin/activate
 pip3 install scrapegraphai
 python3 -m playwright install
---
+++ mpgs_with_openai.py
@@ -0,0 +1,29 @@
+import os
+
+from scrapegraphai.graphs import SmartScraperGraph
+
+# 从环境变量中获取 OpenAI API Key
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+graph_config = {
+    "llm": {
+        "api_key": "OPENAI_API_KEY",
+        "model": "openai/gpt-4o",
+    },
+    "tts_model": {
+        "api_key": "OPENAI_API_KEY",
+        "model": "tts-1",
+        "voice": "alloy"
+    },
+    "output_path": "audio_summary.mp3",
+}
+
+smart_scraper_graph = SmartScraperGraph(
+    prompt="List all the protocols that are supported by the API",
+    # 也接受已下载的 HTML 代码的字符串
+    source="https://ap-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/100/operation/Transaction:%20%20Authorize.html?locale=en_US",
+    config=graph_config
+)
+
+result = smart_scraper_graph.run()
+print(result)
